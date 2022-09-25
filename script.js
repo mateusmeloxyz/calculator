9737fad5afcc06b9b0b77673880f0a0d4ad6c214
mateusgomes01 <melo.mateus@proton.me>
2022-09-24 21:20:53 -0300

    Create operate function

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ function prod(a,b){
 function div(a,b){
     return a/b;
 }
+
+function operate(op,a,b){
+    switch(op){
+        case '+':
+            return add(a,b);
+            break;
+        case '-':
+            return sub(a,b);
+            break;
+        case '*':
+        case 'x':
+        case 'X':
+            return prod(a,b);
+            break;
+        case '/':
+            return div(a,b);
+            break;
+        default:
+            return 'err';
+            break;
+    }
+
+    return 'err';
+}
