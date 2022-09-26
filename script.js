14b945fa39b3929251b11ac2ff8a6b0a9b2fe479
mateusgomes01 <melo.mateus@proton.me>
2022-09-26 17:32:40 -0300

    Make basic operations and clear work

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+let displayValue = '0';
+let currOp = '';
+let num1 = '';
+let num2 = '';
+
+function begin() {
+    display.textContent = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '0';
+    display.textContent = displayValue;
+}
+
 function add(a,b){
-    return a+b;
+    return +a + +b;
 }
 
 function sub(a,b){
@@ -37,3 +51,34 @@ function operate(op,a,b){
 
     return 'err';
 }
+
+const numberKeys = document.getElementById("number-keys");
+const funcKeys = document.getElementById("function-keys");
+
+const display = document.getElementById("display")
+
+for (const numKey of numberKeys.children ) {
+    numKey.addEventListener("click", (e) => {
+        displayValue = displayValue + e.target.textContent;
+        display.textContent = displayValue;
+    })
+
+}
+
+for (const funcKey of funcKeys.children ) {
+    funcKey.addEventListener("click", (e) => {
+        if( e.target.textContent === '='){
+            displayValue = operate(currOp, num1, displayValue);
+        } else if(e.target.textContent === 'c'){
+            clearDisplay();
+        } else {
+            currOp = e.target.textContent;
+            num1 = displayValue;
+            clearDisplay();
+        }
+        display.textContent = displayValue;
+    })
+
+}
+
+begin();
